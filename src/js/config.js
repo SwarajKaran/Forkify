3b31ccf3ef34d23f1b2085ed3fc82faade80e122
SwarajKaran <swarajkaran.raj@gmail.com>
2022-09-20 01:39:54 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '381e70cc-0a4a-41e8-831d-be3a2731f5b7';
+export const MODAL_CLOSE_SEC = 2.5;
